f48bb7d5c184e18c8aa2e4509b00efa77b0cca6e
wildanelek12 <38659848+wildanelek12@users.noreply.github.com>
2021-06-21 07:18:50 +0700

    as

--- app/src/main/java/com/codesid/gogrin/LoginActivity.java
+++ app/src/main/java/com/codesid/gogrin/LoginActivity.java
@@ -1,14 +1,96 @@
 package com.codesid.gogrin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class LoginActivity extends AppCompatActivity {
-
+    @BindView(R.id.et_username)
+    EditText et_username;
+    @BindView(R.id.et_password)
+    EditText et_password;
+    @BindView(R.id.btn_login)
+    ImageView btn_login;
+    private FirebaseAuth mAuth;
+    String email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = et_username.getText().toString();
+                password = et_password.getText().toString();
+                if (email.equals("")&&password.equals("")){
+                    et_username.setError("Isi Terlebih Dahulu");
+                    et_password.setError("Isi Terlebih Dahulu");
+                }else if (email.equals("")){
+                    et_username.setError("Isi Terlebih Dahulu");
+                }else if (password.equals("")){
+                    et_password.setError("Isi Terlebih Dahulu");
+                }
+                else {
+                    login(email,password);
+                }
+            }
+        });
+
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(this,MainMenuActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void login(String email,String password){
+        SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText("Loading");
+        pDialog.setCancelable(false);
+        pDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this,MainMenuActivity.class);
+                            startActivity(intent);
+                        } else {
+                            new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.ERROR_TYPE)
+                                    .setTitleText("Oops...")
+                                    .setContentText("Something went wrong!")
+                                    .show();
+                        }
+                    }
+                });
     }
 }
